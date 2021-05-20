4dc9b74af70f536cbf0ecd781441f7297ab525c1
Stepg05 <stepgambino@gmail.com>
2021-05-20 09:40:11 +0200

    test commit

--- src/main/java/com/example/model/Nota.java
+++ src/main/java/com/example/model/Nota.java
@@ -1,10 +1,21 @@
 package com.example.model;
 
+
 import javax.persistence.*;
 
 import lombok.Data;
 
 import java.io.Serializable;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
 @Table(name="nota")
@@ -15,12 +26,14 @@ public class Nota implements Serializable{
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@Column
 	private String title;
-	
+
 	@Column
 	private String description;
-	
-	
+
+
+
+
 }
